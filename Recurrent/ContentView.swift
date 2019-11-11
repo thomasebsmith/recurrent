9edb3247b4e3eb0c9b628bf05c8019d9a816bf42
Thomas Smith <thsm@umich.edu>
2019-11-11 00:00:21 -0500

    Initial Commit

---
+++ Recurrent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recurrent
+//
+//  Created by Thomas Smith on 11/11/19.
+//  Copyright © 2019 Thomas Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
