4acd871e05f90bb132393b763728418b84c6577d
Thomas Smith <thsm@umich.edu>
2019-12-07 14:49:07 -0500

    Implement Core Data interface for storing new data

--- Recurrent/Models/ActivityManager.swift
+++ Recurrent/Models/ActivityManager.swift
@@ -10,9 +10,31 @@ import CoreData
 import UIKit
 
 class ActivityManager {
+    // MARK: - Class members and constants
     private var oldActivities: [Date: [Activity]]
     private var newActivities: [Date: [Activity]]
-    private func add(activity: Activity, to array: inout [Activity]?) {
+    private static let activityStorageName = "ActivityData"
+    private static let activityStorageKey = "activity"
+    private static let oldStorageName = "oldActivities"
+    private static let newStorageName = "newActivities"
+
+    // MARK: - Private methods and getters
+    private var activityDateCutoff: Date {
+        Calendar.current.startOfDay(for: Date())
+    }
+    private func storageDate(for activity: Activity) -> Date {
+        return Calendar.current.startOfDay(for: activity.date.end)
+    }
+    private func isNew(_ activity: Activity) -> Bool {
+        return storageDate(for: activity) >= activityDateCutoff
+    }
+    private func storageName(for activity: Activity) -> String {
+        if isNew(activity) {
+            return type(of: self).newStorageName
+        }
+        return type(of: self).oldStorageName
+    }
+    private func add(_ activity: Activity, to array: inout [Activity]?) {
         if array != nil {
             array?.append(activity)
         }
@@ -32,9 +54,7 @@ class ActivityManager {
             }
             for activityData in list {
                 if let activity = activityData {
-                    let endDate = activity.date.end
-                    let endDay = Calendar.current.startOfDay(for: endDate)
-                    add(activity: activity, to: &dict[endDay])
+                    add(activity, to: &dict[storageDate(for: activity)])
                 }
             }
         }
@@ -42,10 +62,34 @@ class ActivityManager {
             return
         }
     }
+
+    // MARK: - Public interface
+    func add(activity: Activity) -> Bool {
+        guard let app = UIApplication.shared.delegate as? AppDelegate else {
+            return false
+        }
+        guard let entity = NSEntityDescription.entity(
+            forEntityName: type(of: self).activityStorageName,
+            in: app.persistentContainer.viewContext
+        ) else {
+                return false
+        }
+        let object = ActivityData(
+            entity: entity,
+            insertInto: app.persistentContainer.viewContext
+        )
+        object.setValue(activity, forKey: type(of: self).activityStorageKey)
+        do {
+            try app.persistentContainer.viewContext.save()
+        } catch {
+            return false
+        }
+        return true
+    }
     init() {
         oldActivities = [:]
         newActivities = [:]
-        add(data: "oldActivities", to: &oldActivities)
-        add(data: "newActivities", to: &newActivities)
+        add(data: type(of: self).oldStorageName, to: &oldActivities)
+        add(data: type(of: self).newStorageName, to: &newActivities)
     }
 }
